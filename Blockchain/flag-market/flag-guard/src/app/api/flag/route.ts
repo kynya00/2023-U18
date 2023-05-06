3419de05c91819c453b14a84078428caa1ac1693
Zolboobayar Gantumur <zolboobayar.gantumur@owasp.org>
2023-05-06 19:54:31 +0300

    feat(flag-guard): add flag API

---
+++ Blockchain/flag-market/flag-guard/src/app/api/flag/route.ts
@@ -0,0 +1,33 @@
+import { ethers } from "ethers";
+import { NextResponse } from "next/server";
+
+import { env } from "@/env";
+
+const digest = ethers.hashMessage("flag-market-challenge");
+const provider = new ethers.JsonRpcProvider(env.RPC_URL);
+const contract = new ethers.Contract(
+  env.CONTRACT_ADDRESS,
+  ["function hasBoughtFlag(address user) public view returns (bool)"],
+  provider
+);
+
+export async function POST(request: Request) {
+  const body = await request.json();
+
+  let address: string;
+  try {
+    address = ethers.recoverAddress(digest, body);
+  } catch {
+    return new NextResponse("Don't try to fool me!! https://http.cat/403", {
+      status: 403,
+    });
+  }
+
+  const hasBoughtFlag = await contract.hasBoughtFlag(address);
+  if (!hasBoughtFlag) {
+    return new NextResponse("Don't try to fool me!! https://http.cat/403", {
+      status: 403,
+    });
+  }
+  return NextResponse.json({ flag: env.FLAG });
+}

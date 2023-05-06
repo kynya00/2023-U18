4ed9d53bb2305e8f4637d82898039cf4633468b9
Zolboobayar Gantumur <zolboobayar.gantumur@owasp.org>
2023-05-06 20:57:23 +0300

    feat(flag-guard): add requester implementation

--- Blockchain/flag-market/flag-guard/src/app/page.tsx
+++ Blockchain/flag-market/flag-guard/src/app/page.tsx
@@ -1,5 +1,13 @@
+import { challenge } from "./api/flag/route";
+
+import { FlagRequestWidget } from "@/components/FlagRequestWidget";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <FlagRequestWidget
+        challenge={"0x" + Buffer.from(challenge, "utf-8").toString("hex")}
+      />
+    </main>
   );
 }

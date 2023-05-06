4dd6d1251e1ce1a06502aa0774d21463767ec37c
Zolboobayar Gantumur <zolboobayar.gantumur@owasp.org>
2023-05-06 19:26:07 +0300

    feat: implement faucet form

--- Blockchain/faucet/src/app/page.tsx
+++ Blockchain/faucet/src/app/page.tsx
@@ -1,7 +1,9 @@
+import { FaucetForm } from "@/components/FaucetForm";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <p>Hello there!</p>
+      <FaucetForm />
     </main>
   );
 }

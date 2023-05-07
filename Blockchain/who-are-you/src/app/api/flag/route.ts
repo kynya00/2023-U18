7fdc3faa807add15c73424d1c0f39250a023dffe
Zolboobayar Gantumur <zolboobayar.gantumur@owasp.org>
2023-05-07 08:35:00 +0300

    feat(blockchain): add who-are-you challenge

---
+++ Blockchain/who-are-you/src/app/api/flag/route.ts
@@ -0,0 +1,26 @@
+import { ethers } from "ethers";
+import { NextResponse } from "next/server";
+
+import { env } from "@/env";
+
+export const challenge = "HaruulZangiU18";
+
+const digest = ethers.hashMessage(challenge);
+
+export async function POST(request: Request) {
+  const body = await request.formData();
+
+  let address: string;
+  try {
+    address = ethers.recoverAddress(digest, body.get("signature") as string);
+    if (address !== body.get("address")) {
+      throw Error("Address mismatch");
+    }
+  } catch {
+    return new NextResponse("Don't try to fool me!! https://http.cat/403", {
+      status: 403,
+    });
+  }
+
+  return NextResponse.json({ flag: env.FLAG });
+}
---
+++ Blockchain/who-are-you/src/env.ts
@@ -0,0 +1,7 @@
+import { z } from "zod";
+
+export const env = z
+  .object({
+    FLAG: z.string().regex(/^HZU18{.+}$/),
+  })
+  .parse(process.env);

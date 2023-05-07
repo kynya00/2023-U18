914653eca5ac99e0508daecadf2fa8dbc8f2913b
Zolboobayar Gantumur <zolboobayar.gantumur@owasp.org>
2023-05-07 10:02:05 +0300

    feat(massignment): perists db to fs

--- Web/massignment/src/server/db.ts
+++ Web/massignment/src/server/db.ts
@@ -5,14 +5,18 @@ let db: Database;
 export async function getDB() {
   if (!db) {
     db = await open({
-      filename: ":memory:",
+      filename: "data.sqlite",
       driver: sqlite3.Database,
     });
-    await db.exec(
-      "CREATE TABLE users (id TEXT NOT NULL, username TEXT NOT NULL, password TEXT NOT NULL, role TEXT NOT NULL)"
-    );
-    await db.exec("CREATE UNIQUE INDEX idx_users_id ON users (id)");
-    await db.exec("CREATE UNIQUE INDEX idx_users_username ON users (username)");
+    try {
+      await db.exec(
+        "CREATE TABLE users (id TEXT NOT NULL, username TEXT NOT NULL, password TEXT NOT NULL, role TEXT NOT NULL)"
+      );
+      await db.exec("CREATE UNIQUE INDEX idx_users_id ON users (id)");
+      await db.exec(
+        "CREATE UNIQUE INDEX idx_users_username ON users (username)"
+      );
+    } catch {}
   }
   return db;
 }

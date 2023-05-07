64ec2ddd974bbd0297a3f93bde47be9a471f8eeb
Zolboobayar Gantumur <zolboobayar.gantumur@owasp.org>
2023-05-07 15:18:13 +0300

    fix(massignment): output standalone bundle

--- Web/massignment/next.config.js
+++ Web/massignment/next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { experimental: { serverActions: true } };
+const nextConfig = {
+  output: "standalone",
+  experimental: { serverActions: true },
+};
 
 module.exports = nextConfig;
